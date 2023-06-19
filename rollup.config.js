94a9939829a6d18cbd1162118abb990e59e4a99a
keyurparalkar <keur.plkar@gmail.com>
2023-06-19 09:51:05 +0530

    chore: reorganized the folder structure

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import typescript from "rollup-plugin-typescript2";
 const packageJson = require("./package.json");
 
 export default {
-  input: "lib/index.ts",
+  input: "package/lib/index.ts",
   output: [
     {
       file: packageJson.module,
