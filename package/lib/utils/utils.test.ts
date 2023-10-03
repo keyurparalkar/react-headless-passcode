51e3cc9811ecba917410106758bef14892b5d060
keyurparalkar <keur.plkar@gmail.com>
2023-10-03 11:36:32 +0530

    test: added unit test for utils shouldPreventDefault

---
+++ package/lib/utils/utils.test.ts
@@ -0,0 +1,31 @@
+import { shouldPreventDefault } from "./index";
+
+describe("test shouldPreventDefault", () => {
+    test("1. Should return false if key that is pressed is: Ctrl + V, Backspace, and digits 1-9 when isAlphaNumeric = false", () => {
+        let key = "1";
+        expect(shouldPreventDefault(key)).toBeFalsy();
+
+        key = "v";
+        expect(shouldPreventDefault(key, false, true)).toBeFalsy();
+
+        key = "Backspace";
+        expect(shouldPreventDefault(key)).toBeFalsy();
+
+        key = "a";
+        expect(shouldPreventDefault(key)).toBeTruthy();
+    });
+
+    test("2. Should return false if key that is pressed is: Ctrl + V, Backspace, and digits 1-9 when isAlphaNumeric = true", () => {
+        let key = "1";
+        expect(shouldPreventDefault(key, true)).toBeFalsy();
+
+        key = "v"; // Ctrl + v
+        expect(shouldPreventDefault(key, true, true)).toBeFalsy();
+
+        key = "Backspace";
+        expect(shouldPreventDefault(key, true)).toBeFalsy();
+
+        key = "a";
+        expect(shouldPreventDefault(key, true)).toBeFalsy();
+    });
+});
